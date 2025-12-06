f2c28f296ff70ccc5b3a494d473b34ac6ca89fa3
Paulo <pompompetesteticaanimal@gmail.com>
2025-12-06 19:00:38 -0300

    remocao de IA_index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Aumenta o limite de aviso para 1600kb para evitar warnings amarelos no build
+    chunkSizeWarningLimit: 1600,
+  },
 })
